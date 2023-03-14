32e30e6de621a3136532734b18a9ff7eff62ae30
carperbr <carperbr@gmail.com>
2023-03-14 17:37:38 -0400

    some updates to lyapunov calculation

--- lyapunov-test.py
+++ lyapunov-test.py
@@ -1,58 +1,47 @@
-from scipy.integrate import RK45
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from systems import van_der_pol_oscillator, forced_conservative_nonlinear_oscillator05, forced_van_der_pol_oscillator, build_system
 
-vs = []
-xs = []
+def calculate_lyapunov_exponent(params, state, t0, system):
+    solver = build_system(system, params, state, t0)
 
-params = []
-state = [0, 0]
-t0 = 0
-system = forced_conservative_nonlinear_oscillator05
+    dt0 = 0
+    while dt0 < 1000:
+        solver.step()
+        dt0 = dt0 + solver.step_size
 
-solver = build_system(system, params, state, t0)
+    pstate = np.copy(solver.y)
+    pstate[0] = pstate[0] + np.random.randn() * 1e-12
+    solver2 = build_system(system, params, pstate, solver.t)
+    lsum = 0
 
-dt0 = 0
-while dt0 < 1000:
-    solver.step()
-    dt0 = dt0 + solver.step_size
+    dt = 0
+    dt2 = 0
+    timesteps = 100000
+    for i in tqdm(range(timesteps)):
+        solver.step()
+        solver2.step()
 
-pstate = np.copy(solver.y)
-pstate[0] = pstate[0] + np.random.randn() * 1e-12
+        v = solver2.y - solver.y
+        d = np.linalg.norm(v)
+        rs = 1 / np.sqrt(d)
+        solver2.y = solver.y + rs * (solver2.y - solver.y)
+        lsum = lsum + np.log(d)
 
-solver2 = build_system(system, params, pstate, solver.t)
-lsum = 0
+        r = (np.cos((v[0]) * np.pi) + 1) / 2.0
+        g = (np.cos((v[1]) * np.pi) + 1) / 2.0
+        b = (np.sin((v[0]) * np.pi) + 1) / 4.0 + (np.sin((v[1]) * np.pi) + 1) / 4.0
 
-dt = 0
-dt2 = 0
-timesteps = 100000
-for i in tqdm(range(timesteps)):
-    solver.step()
-    solver2.step()
+        plt.plot([solver.y[0], solver.y_old[0]], [solver.y[1], solver.y_old[1]], linewidth=0.25, color=(r,g,b), alpha=0.2)
 
-    v = solver2.y - solver.y
-    d = np.linalg.norm(solver2.y - solver.y)
-    rs = 1 / np.sqrt(d)
-    solver2.y = solver.y + rs * (solver2.y - solver.y)
-    lsum = lsum + np.log(d)
+        dt = dt + solver.step_size
+        dt2 = dt2 + solver2.step_size
 
-    r = (np.cos((v[0]) * np.pi) + 1) / 2.0
-    g = (np.cos((v[1]) * np.pi) + 1) / 2.0
-    b = (np.sin((v[0]) * np.pi) + 1) / 4.0 + (np.sin((v[1]) * np.pi) + 1) / 4.0
+    l = lsum / dt
+    plt.savefig(f'tmp{np.random.randn()}.png', dpi=512)
+    
+    return np.max(l)
 
-    plt.plot([solver.y[0], solver.y_old[0]], [solver.y[1], solver.y_old[1]], linewidth=0.25, color=(r,g,b), alpha=0.2)
-
-    dt = dt + solver.step_size
-    dt2 = dt2 + solver2.step_size
-
-l = lsum / dt
-
-print(dt)
-print(dt2)
-
-largest = np.max(l)
-print(l)
-plt.savefig(f'tmp{np.random.randn()}.png', dpi=512)
+print(calculate_lyapunov_exponent([], [0, 0], 0, forced_conservative_nonlinear_oscillator05))
